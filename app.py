71b7ef65953d6a4aec13ba0e57597aa7e158239e
jacob <angell.zhfg@gmail.com>
2021-04-19 13:06:50 +0800

    add approval

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import requests
+
 
 app = Flask(__name__)
 
@@ -7,6 +9,18 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+
+@app.route('/<approval_url>')
+def approval_url(approval_url):
+    if approval_url is None:
+        print("NONE_APPROVAL_URL")
+        return 'OK'
+    return(
+        requests.Post(
+            approval_url,
+            data="{'data':'ok'}"
+        )
+    )
 
 if __name__ == '__main__':
     app.run()
